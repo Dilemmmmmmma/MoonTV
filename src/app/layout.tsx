f9ee9910d6d3110791859855890f6a5fdac14300
FourFreak <69084711+Dilemmmmmmma@users.noreply.github.com>
2026-02-16 13:38:56 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,6 +29,7 @@ export async function generateMetadata(): Promise<Metadata> {
   return {
     title: siteName,
     description: '影视聚合',
+    referrer: 'no-referrer',
     manifest: '/manifest.json',
   };
 }
